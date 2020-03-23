c6fbb883329e8fedb1f8a6ce1984a5cefd2ca0bd
nebblu <45853389+nebblu@users.noreply.github.com>
2020-03-23 11:51:46 +0100

    Update SpecialFunctions.cpp
    Changed mg to p1 in mymgF

--- reactions/src/SpecialFunctions.cpp
+++ reactions/src/SpecialFunctions.cpp
@@ -263,12 +263,11 @@ double mymgF(double a, double yh, double yenv, double Rth, double omega0, double
 
 /*DGP normal branch*/
 		//       double betadgp,xm3,xterm,delta;
-		//       betadgp = beta(a,omega0,mg);
-		//       double ai = exp(T00);
+		//       betadgp = beta(a,omega0,p1);
 		//
 		//       delta = (1.+delta_initial)/pow3(yh) - 1.;
 		//
-		//       xterm = 9.*pow2(betadgp)*mg/(2.*omega0/pow3(a)*delta);
+		//       xterm = 9.*pow2(betadgp)*p1/(2.*omega0/pow3(a)*delta);
 		//
 		//       xm3 = 1./xterm;
 		//

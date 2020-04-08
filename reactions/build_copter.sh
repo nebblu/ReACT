806e5efc17ed935edbd7695f4787a7832911733a
Tilman Troester <tilman.troester@gmail.com>
2020-04-08 15:34:21 +0100

    Support gcc on mac OS.

--- reactions/build_copter.sh
+++ reactions/build_copter.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-./configure --prefix=`pwd` LDFLAGS="${LDFLAGS}" CFLAGS="${CFLAGS} -w" CXXFLAGS="${CXXFLAGS} -g -std=c++11" LIBS="-lgsl -lstdc++ -lsundials_cvode -lsundials_nvecserial"
-
+./configure --prefix=`pwd` LDFLAGS="${LDFLAGS}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}"
 echo "Running make"
 make
 make install

c41f3368c216aaa59f9f521beb53aa6ab6e10e79
Benjamin Bose <nebblub@gmail.com>
2020-09-17 10:04:44 +0200

    Corrected dimension of array

--- reactions/src/HALO.cpp
+++ reactions/src/HALO.cpp
@@ -232,7 +232,7 @@ double sig1 = Integrate<ExpSub>(bind(sigma_integrandp, cref(P_l), 8., std::place
 double sig2 = Integrate<ExpSub>(bind(sigma8d_integrandp, cref(P_l), 8., std::placeholders::_1), 1e-4, 50., 1e-3);
 
 // feed back modified sigma8
-    vars[7] = sqrt(sig1);
+    vars[6] = sqrt(sig1);
 
    // theory params
    double pars[4];

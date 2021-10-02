63902373d76d542d204b246be08e7084ec2d5d54
Benjamin Bose <>
2021-10-02 17:17:25 +0200

    upgraded python wrapper to include massive neutrino initialisation: compute_reaction_nu. Added new pyreact function to call this function

--- pyreact/react.py
+++ pyreact/react.py
@@ -36,11 +36,13 @@ class ReACT:
         r = f(*array_arg(a))
         return r
 
+# Compute reaction without massive neutrinos
     def compute_reaction(self, h, n_s, omega_m, omega_b, sigma_8,
                                z, k, Pk,
                                model="f(r)", fR0=None, Omega_rc=None, w=None, wa=None,
                                is_transfer=False, mass_loop=30,
                                verbose=True):
+
 
         if max(z) > 2.5:
             raise ValueError("ReACT is unstable above z=2.5, try limiting the range of z values.")
@@ -52,22 +54,27 @@ class ReACT:
             reaction_model = 2
             modified_gravity_param = fR0
             modified_gravity_param2 = 0.0
+            modified_gravity_param3 = 0.0
         elif model.lower() == "dgp":
             reaction_model = 3
             modified_gravity_param = Omega_rc
             modified_gravity_param2 = 0.0
+            modified_gravity_param3 = 0.0
         elif model.lower() == "gr":
             reaction_model = 1
             modified_gravity_param = 0.0
             modified_gravity_param2 = 0.0
+            modified_gravity_param3 = 0.0
         elif model.lower() == "quintessence":
             reaction_model = 4
             modified_gravity_param = w
             modified_gravity_param2 = 0.0
+            modified_gravity_param3 = 0.0
         elif model.lower() == "cpl":
             reaction_model = 5
             modified_gravity_param = w
             modified_gravity_param2 = wa
+            modified_gravity_param3 = 0.0
         else:
             raise ValueError(f"model '{model}' not supported.")
 
@@ -87,6 +94,7 @@ class ReACT:
                       ct.POINTER(ct.c_double),     # sigma_8
                       ct.POINTER(ct.c_double),     # model parameter 1 : for f(R) this is fr0, for dgp this is Omega_rc, for CPL or quintessence this is w0
                       ct.POINTER(ct.c_double),     # model parameter 2 : for CPL this is wa
+                      ct.POINTER(ct.c_double),     # model parameter 3
                       ct.POINTER(ct.c_int),        # mass_loop
                       ct.POINTER(ct.c_int),        # model (1: GR, 2: f(R), 3; DGP, 4: quintessence, 5: CPL)
                       *array_ctype(ndim=2, dtype=np.float64), # reaction (output)
@@ -105,6 +113,7 @@ class ReACT:
                 ct.c_double(h), ct.c_double(n_s), ct.c_double(omega_m), ct.c_double(omega_b), ct.c_double(sigma_8),
                 ct.c_double(modified_gravity_param),
                 ct.c_double(modified_gravity_param2),
+                ct.c_double(modified_gravity_param3),
                 ct.c_int(mass_loop),
                 ct.c_int(reaction_model),
                 *array_arg(reaction),
@@ -118,4 +127,110 @@ class ReACT:
             raise RuntimeError(f"ReACT code terminated with an error: {error_message}")
         # Get into CAMB ordering (z, k), with increasing z
         #Tilman: to check output of modsig8
+        return reaction[:,::-1].T, p_lin[:,::-1].T, sigma8[0]
+
+
+
+
+    def compute_reaction_nu(self, h, n_s, omega_m, omega_b, omega_nu, As,
+                               z, k, Tm, Tcb, klcdm, Tcblcdm,
+                               pscale = 0.05,
+                               model="f(r)", fR0=None, Omega_rc=None, w=None, wa=None,
+                               is_transfer=False, mass_loop=30,
+                               verbose=True):
+
+        if max(z) > 2.5:
+            raise ValueError("ReACT is unstable above z=2.5, try limiting the range of z values.")
+
+        if len(z) > 1 and z[0] > z[-1]:
+            raise ValueError("The z array needs to be ordered from low to high redshift.")
+
+        if model.lower() == "f(r)":
+            reaction_model = 2
+            modified_gravity_param = fR0
+            modified_gravity_param2 = 0.0
+            modified_gravity_param3 = 0.0
+        elif model.lower() == "dgp":
+            reaction_model = 3
+            modified_gravity_param = Omega_rc
+            modified_gravity_param2 = 0.0
+            modified_gravity_param3 = 0.0
+        elif model.lower() == "gr":
+            reaction_model = 1
+            modified_gravity_param = 0.0
+            modified_gravity_param2 = 0.0
+            modified_gravity_param3 = 0.0
+        elif model.lower() == "quintessence":
+            reaction_model = 4
+            modified_gravity_param = w
+            modified_gravity_param2 = 0.0
+            modified_gravity_param3 = 0.0
+        elif model.lower() == "cpl":
+            reaction_model = 5
+            modified_gravity_param = w
+            modified_gravity_param2 = wa
+            modified_gravity_param3 = 0.0
+        else:
+            raise ValueError(f"model '{model}' not supported.")
+
+        if modified_gravity_param is None:
+            raise ValueError("fR0, Omega_rc or w0 need to be specified.")
+
+
+        f = self.get_function("compute_reaction_nu")
+        f.restype = np.int
+        f.argtypes = [*array_ctype(ndim=1, dtype=np.float64), # full matter transfer
+                      *array_ctype(ndim=1, dtype=np.float64), # k
+                      *array_ctype(ndim=1, dtype=np.float64), # z
+                      *array_ctype(ndim=1, dtype=np.float64), # CDM + baryon transfer
+                      *array_ctype(ndim=1, dtype=np.float64), # LCDM CDM+baryon transfer
+                      *array_ctype(ndim=1, dtype=np.float64), # k for LCDM transfer
+                      ct.POINTER(ct.c_bool),       # is_transfer
+                      ct.POINTER(ct.c_double),     # h
+                      ct.POINTER(ct.c_double),     # n_s
+                      ct.POINTER(ct.c_double),     # omega_m
+                      ct.POINTER(ct.c_double),     # omega_b
+                      ct.POINTER(ct.c_double),     # omega_nu
+                      ct.POINTER(ct.c_double),     # A_s
+                      ct.POINTER(ct.c_double),     # pscale
+                      ct.POINTER(ct.c_double),     # model parameter 1 : for f(R) this is fr0, for dgp this is Omega_rc, for CPL or quintessence this is w0
+                      ct.POINTER(ct.c_double),     # model parameter 2 : for CPL this is wa
+                      ct.POINTER(ct.c_double),     # model parameter 3 : for DS this is xi * h
+                      ct.POINTER(ct.c_int),        # mass_loop
+                      ct.POINTER(ct.c_int),        # model (1: GR, 2: f(R), 3; DGP, 4: quintessence, 5: CPL)
+                      *array_ctype(ndim=2, dtype=np.float64), # reaction (output)
+                      *array_ctype(ndim=2, dtype=np.float64), # linear MG power spectrum (output)
+                      np.ctypeslib.ndpointer(ndim=1, dtype=np.float64, flags="C"),     # modified sigma_8 storage variable
+                      ct.POINTER(ct.c_int),        # verbose
+                     ]
+
+        reaction = np.zeros((len(k), len(z)), dtype=k.dtype, order="C")
+        p_lin = np.zeros((len(k), len(z)), dtype=k.dtype, order="C")
+        sigma8 = np.zeros(1, dtype=k.dtype, order="C")
+
+
+        r =   f(*array_arg(np.ascontiguousarray(Tm, dtype=np.float64)),
+                *array_arg(np.ascontiguousarray(k, dtype=np.float64)),
+                *array_arg(np.ascontiguousarray(z[::-1], dtype=np.float64)), # ReACT expect z order from high to low
+                *array_arg(np.ascontiguousarray(Tcb, dtype=np.float64)),
+                *array_arg(np.ascontiguousarray(Tcblcdm, dtype=np.float64)),
+                *array_arg(np.ascontiguousarray(klcdm, dtype=np.float64)),
+                ct.c_bool(is_transfer),
+                ct.c_double(h), ct.c_double(n_s), ct.c_double(omega_m), ct.c_double(omega_b), ct.c_double(omega_nu),
+                ct.c_double(As), ct.c_double(pscale),
+                ct.c_double(modified_gravity_param),
+                ct.c_double(modified_gravity_param2),
+                ct.c_double(modified_gravity_param3),
+                ct.c_int(mass_loop),
+                ct.c_int(reaction_model),
+                *array_arg(reaction),
+                *array_arg(p_lin),
+                sigma8,
+                ct.c_int(verbose),
+                )
+        if r != 0:
+            string_type = ct.c_char * ct.c_int.in_dll(self.lib, "ERROR_MESSAGE_LEN").value
+            error_message = string_type.in_dll(self.lib, "error_message").value.decode()
+            raise RuntimeError(f"ReACT code terminated with an error: {error_message}")
+
         return reaction[:,::-1].T, p_lin[:,::-1].T, sigma8[0]
--- pyreact/react_wrapper.cpp
+++ pyreact/react_wrapper.cpp
@@ -25,6 +25,16 @@ extern "C" {
     extern const int ERROR_MESSAGE_LEN = 512;
     char error_message[ERROR_MESSAGE_LEN];
 
+    void react_error_numerical(double val1 = 0, double val2  = 0,double  val3 = 0, double  val4 = 0, double val5 = 0) {
+        const int truncate = ERROR_MESSAGE_LEN-10;
+
+        #pragma omp critical
+        {
+            snprintf(error_message, truncate, "Reaction error: %e %e %e %e %e ", val1, val2, val3, val4, val5);
+            fprintf(stderr, " %e %e %e %e %e \n", val1, val2,val3,val4,val5);
+        }
+    }
+
     void react_error(const char* msg) {
         const int truncate = ERROR_MESSAGE_LEN-10;
 
@@ -49,12 +59,14 @@ extern "C" {
         return 42;
     }
 
+
+    /* Option 1: Run wrapper as normally, i.e. if input transfer/pofk is LCDM at z=0 */
     int compute_reaction(int* N_k_pk, double* powerspectrum,
                          int* N_k, double* kvals,
                          int* N_z, double* zvals,
                          bool* is_transfer,
                          double* h, double* n_s, double* Omega_m, double* Omega_b, double* sigma_8,
-                         double* mg1, double* mg2, int* mass_loop, int* model,
+                         double* mg1, double* mg2, double* mg3, int* mass_loop, int* model,
                          int* N_k_react, int* N_z_react, double* output_react,
                          int* N_k_pl, int* N_z_pl, double* output_pl,
                          double* modsig8,
@@ -94,6 +106,7 @@ extern "C" {
             std::cout<<"sigma_8: " << *sigma_8 << "\n";
             std::cout<<"mg1: " << *mg1 << "\n";
             std::cout<<"mg2: " << *mg2 << "\n";
+            std::cout<<"mg3: " << *mg3 << "\n";
             std::cout<<"mass loop: " << *mass_loop << "\n";
             std::cout<<"model: "  << *model << " (1:GR, 2:f(R), 3:DGP, 4:quintessence, 5: CPL)\n";
 
@@ -146,11 +159,11 @@ extern "C" {
         vars[1] = *Omega_m;
         vars[2] = *mg1;
         vars[3] = *mg2;
-        vars[4] = 1.;
+        vars[4] = *mg3;
         vars[5] = *mass_loop;
         vars[6] = 0.0; // massive neutrino mass sum, not implemented in pyreact yet!
 
-        bool mgcamb = false;
+        bool modcamb = false;
         int mod = *model;
         // initialise power spectrum normalisation before running 1-loop computations
         iow.initnorm(vars,mod);
@@ -190,12 +203,12 @@ extern "C" {
             iow.initnorm(vars,mod);
             // Spherical collapse stuff
             /// initialise delta_c(M), a_vir(M), delta_avir(M) and v(M)
-            status = halo.scol_init(vars,mgcamb,mod);
-            status |= halo.scol_initp(vars,mgcamb);
+            status = halo.scol_init(vars,modcamb,mod);
+            status |= halo.scol_initp(vars,modcamb);
 
             // store modified sigma 8 at z=0
             if(j == *N_z-1) {
-                modsig8[0] = vars[6];
+                modsig8[0] = vars[7];
             }
 
             if(status != 0) {
@@ -204,7 +217,7 @@ extern "C" {
             }
 
             // initialise k_star and mathcal{E}
-            halo.react_init2(vars,mysr,mysp,modg);
+            halo.react_init_nu_multiz(vars,mysr,mysp,modcamb,modg);  // kstar and mathcal E for reaction for multiple redshifts
 
             // reaction
             //#pragma omp parallel for
@@ -215,7 +228,7 @@ extern "C" {
                     react_tab[i] = 1.;
                 }
                 else {
-                    react_tab[i] = halo.reaction_nu(k2val[i], vars, mgcamb);
+                    react_tab[i] = halo.reaction_nu(k2val[i], vars, modcamb);
                 }
             }
 
@@ -223,7 +236,7 @@ extern "C" {
 
             for(int i =0; i < *N_k;  i ++) {
                 output_react[i*(*N_z)+j] =  myreact(kvals[i]);
-                output_pl[i*(*N_z)+j] = pow2(halo.Lin_Grow(kvals[i]))*powerspectrum[i];
+                output_pl[i*(*N_z)+j] = pow2(halo.Lin_Grow(kvals[i]))*P_l(kvals[i]);
                 if(*verbose > 1) {
                     printf(" %e %e %e \n",zvals[j], kvals[i],halo.plinear_cosmosis(kvals[i]));
                 }
@@ -232,4 +245,310 @@ extern "C" {
 
         return 0;
     }
+
+
+    /* Option 2: Run wrapper assuming input is transfer/pofk array in k and z for all species in target cosmology */
+    int compute_reaction_nu(int* N_pk_m, double* torpk_m,
+                         int* N_k, double* kvals,
+                         int* N_z, double* zvals,
+                         int* N_pk_cb, double* torpk_cb,
+                         int* N_pk_lcdm, double* torpk_lcdm,
+                         int* N_k_lcdm, double* kvals_lcdm,
+                         bool* is_transfer,
+                         double* h, double* n_s, double* Omega_m, double* Omega_b, double* Omega_nu,
+                         double* As, double* pscale,
+                         double* mg1, double* mg2, double* mg3, int* mass_loop, int* model,
+                         int* N_k_react, int* N_z_react, double* output_react,
+                         int* N_k_pl, int* N_z_pl, double* output_pl,
+                         double* modsig8,
+                         int* verbose)
+    {
+
+        int status = 0;
+
+        if(*N_k != *N_k_react || *N_k != *N_k_pl || *N_pk_m/(*N_z) != *N_k || *N_pk_cb/(*N_z) != *N_k ){
+            react_error("Inconsistency in k array sizes");
+            return 1;
+        }
+        if(*N_z != *N_z_react || *N_z != *N_z_pl || *N_z != *N_pk_m/(*N_k)){
+            react_error("Inconsistency in z array sizes");
+            return 1;
+        }
+
+        if(kvals[0] > 1e-4) {
+            react_error("k must at least go down to 1e-4");
+            return 1;
+        }
+        if(zvals[0] < zvals[*N_z-1]) {
+            react_error("z must be in decreasing order.");
+            return 1;
+        }
+        if(zvals[0] > 2.5) {
+            react_error("z must be smaller than 2.5");
+            return 1;
+        }
+
+
+        if(*verbose > 0) {
+            std::cout<<"z: " << zvals[0] << " -> " << zvals[*N_z-1] << "\n";
+            std::cout<<"k: " << kvals[0] << " -> " << kvals[*N_k-1] << "\n";
+            std::cout<<"h: " << *h << "\n";
+            std::cout<<"n_s: " << *n_s << "\n";
+            std::cout<<"Omega_m: " << *Omega_m << "\n";
+            std::cout<<"Omega_b: " << *Omega_b << "\n";
+            std::cout<<"Omega_nu: " << *Omega_nu << "\n";
+            std::cout<<"As: " << *As << "\n";
+            std::cout<<"pivot scale: " << *pscale << "\n";
+            std::cout<<"mg1: " << *mg1 << "\n";
+            std::cout<<"mg2: " << *mg2 << "\n";
+            std::cout<<"mg3: " << *mg3 << "\n";
+            std::cout<<"mass loop: " << *mass_loop << "\n";
+            std::cout<<"model: "  << *model << " (1:GR, 2:f(R), 3:DGP, 4:quintessence, 5: CPL)\n";
+        }
+
+
+/* Switch on 1-loop modified gravity correction if DGP or f(R) */
+        bool modg;
+        /* Which model ?*/
+        int mod = *model;
+
+        if (mod == 2 || mod == 3) {
+          modg = true;
+        }
+        else{
+          modg = false;
+        }
+
+        // Relative error in magnitude integrations
+        real epsrel = 1e-2;
+
+/* Initialise arrays for various species */
+        array Tm(*N_k);
+        array Tcb(*N_k);
+        array Tnu(*N_k);
+        array ki(*N_k, kvals);
+
+        /* Perhaps we have a different binning for the LCDM run?*/
+        array Tlcdm(*N_k_lcdm);
+        array kilcdm(*N_k_lcdm, kvals_lcdm);
+
+        /*declare variable array*/
+        double vars[8];
+        vars[0] = 1.;
+        vars[1] = *Omega_m;
+        vars[2] = *mg1;
+        vars[3] = *mg2;
+        vars[4] = *mg3;
+        vars[5] = *mass_loop;
+        vars[6] = *Omega_nu;
+
+        bool modcamb = true;
+
+
+        /* Perform some checks */
+        if ( *pscale<1e-3 || *pscale>0.1 || *As<1.5e-9 || *As>2.5e-9 ) {
+          react_error("Values of As and pivot scale are not compatible -  set 1e-3<pscale<0.1  and 1.5e-9<As<2.5e-9");
+        }
+
+        if (!*is_transfer) {
+          react_error("You must supply the input transfer functions if using a modified linear input");
+        }
+
+
+        // Special function class
+        IOW iow;
+        // initialise power spectrum normalisation before running
+        iow.initnorm(vars,mod);
+
+
+
+/* Option 2: Run wrapper assuming input is transfer/pofk array in k and z for all species in target cosmology */
+/* If transfer or pofk inputs are at the redshifts required and already modified we need to initialise classes for each redshift */
+
+
+          /* PART 1: Initialise vector of classes for each z*/
+
+          // Create vector of cosmology structure
+          vector<Cosmology> mycosmo;
+          mycosmo.reserve((*N_z)*4);
+          // Create vector of classes
+          vector<LinearPS_as> mylps;
+          mylps.reserve((*N_z)*4);
+
+          vector<HALO> myhalo;
+          myhalo.reserve(*N_z);
+
+          vector<SPT> myspt;
+          myspt.reserve(*N_z);
+
+
+          // Load classes for each redshift
+          for(int i=0; i<*N_z; i++){
+
+            // Load transfers for real cosmology
+              for(int j = 0; j<*N_k; j++){
+                  if(*is_transfer) {
+                      Tm[j] = torpk_m[(*N_z-i-1)*(*N_k)+j];
+                      Tcb[j] = torpk_cb[(*N_z-i-1)*(*N_k)+j];
+                      if (*Omega_nu<1e-10) {
+                        Tnu[j] = Tm[j];
+                      }
+                      else{
+                      Tnu[j] = (*Omega_m*Tm[j] - (*Omega_m-*Omega_nu)*Tcb[j])/(*Omega_nu);
+                      }
+                  }
+                  else {
+                      // Copter takes transfer function as input - Easiest fix is to just convert input PS to T
+                      Tm[j] = sqrt(torpk_m[(*N_z-i-1)*(*N_k)+j] / (2*M_PI*M_PI * (*As) * ki[j] * pow(*h,4) * pow(ki[j]*(*h)/(*pscale), *n_s-1.)));
+                      Tcb[j] = sqrt(torpk_cb[(*N_z-i-1)*(*N_k)+j] / (2*M_PI*M_PI * (*As) * ki[j] * pow(*h,4) * pow(ki[j]*(*h)/(*pscale), *n_s-1.)));
+                      if (*Omega_nu<1e-10) {
+                        Tnu[j] = Tm[j];
+                      }
+                      else{
+                      Tnu[j] = (*Omega_m*Tm[j] - (*Omega_m-*Omega_nu)*Tcb[j])/(*Omega_nu);
+                        }
+                    }
+                  }
+
+            // same for LCDM
+                  for(int j = 0; j<*N_k_lcdm; j++){
+                      if(*is_transfer) {
+                        Tlcdm[j] = torpk_lcdm[(*N_z-i-1)*(*N_k_lcdm)+j];
+                      }
+                      else {
+                          // Copter takes transfer function as input - Easiest fix is to just convert input PS to T
+                        Tlcdm[j] = sqrt(torpk_lcdm[(*N_z-i-1)*(*N_k_lcdm)+j] / (2*M_PI*M_PI * (*As) * ki[j] * pow(*h,4) * pow(ki[j]*(*h)/(*pscale), *n_s-1.)));
+                        }
+                      }
+
+                  // Load cosmology classes
+                  mycosmo.push_back(Cosmology(*h, *n_s, *Omega_m, *Omega_b, *As, *pscale, ki, Tm));
+                  mycosmo.push_back(Cosmology(*h, *n_s, *Omega_m, *Omega_b, *As, *pscale, ki, Tcb));
+                  mycosmo.push_back(Cosmology(*h, *n_s, *Omega_m, *Omega_b, *As, *pscale, ki, Tnu));
+                  mycosmo.push_back(Cosmology(*h, *n_s, *Omega_m, *Omega_b, *As, *pscale, kilcdm, Tlcdm));
+
+                  mylps.push_back(LinearPS_as(mycosmo[4*i], 0.));
+                  mylps.push_back(LinearPS_as(mycosmo[4*i+1], 0.));
+                  mylps.push_back(LinearPS_as(mycosmo[4*i+2], 0.));
+                  mylps.push_back(LinearPS_as(mycosmo[4*i+3], 0.));
+
+                  // Load halo class with all linear P(k)
+                  myhalo.push_back(HALO(mycosmo[4*i], mylps[4*i] , mylps[4*i+1], mylps[4*i+2], mylps[4*i+3], epsrel));
+
+                  // Load Standard Perturbation Theory class
+                  myspt.push_back(SPT(mycosmo[4*i+1], mylps[4*i+1], epsrel));
+          }
+
+
+        /* PART 2:  1-loop Perturbation theory part to get k_star */
+
+        /* array set up for 1-loop spt splining over z */
+        double ploopr[*N_z],ploopp[*N_z];
+
+        // initialise splines over redshift for real and pseudo 1-loop spectra @ k0 = 0.06h/Mpc
+        double k0 = 0.06;
+        for(int i=0; i<*N_z; i++) {
+            ploopr[i] = 0.;
+            ploopp[i] = 0.;
+        }
+
+
+      if (modg) {
+        /* Initialise arrays for P_L(k0)  for 1-loop real and pseudo computations */
+        double pkz0[*N_z], pkz0p[*N_z];
+        /* Similiarly for the integrated P_L: P_L (|k-p|) and P_L(p). We integrate with 50 absiccae in cos(theta) = k.p/(|k||p|) and 1600 points in |p| - See src/SPT.cpp for more details */
+        /* Do not change number of abscissae - this is assumed in SPT.cpp's ploop_init_nu function */
+        double pkz1[*N_z][50], pkz2[*N_z][1600], pkz1p[*N_z][50], pkz2p[*N_z][1600];
+
+        // Populate power spectrum arrays
+        double KMAX = QMAXp/k0; // max r = p/k (QMAXp = 30 - see SpecialFunctions.h)
+        double KMIN = QMINp/k0; // min r = p/k (QMINp = 1e-4 - ...)
+
+        double k2val,xv,karg;
+
+        for(int i=0; i<*N_z; i++){
+            pkz0[i] = mylps[i*4+1](k0);
+            pkz0p[i] = mylps[i*4](k0);
+          for(int j=0; j<50; j++){
+              k2val = k0*KMIN * exp(j*log(KMAX/KMIN)/(50.-1.));
+              pkz1[i][j] = mylps[i*4+1](k2val);
+              pkz1p[i][j] = mylps[i*4](k2val);
+                for(int jj=0; jj<32; jj++){
+                  xv = x32[jj];
+                  karg = sqrt(k2val*k2val+k0*k0-2.*k2val*k0*xv);
+                  pkz2[i][jj*50+j] = mylps[i*4+1](karg);
+                  pkz2p[i][jj*50+j] = mylps[i*4](karg);
+            }
+          }
+        }
+
+        /* Initialise 1-loop arrays as function of z @ k0 */
+        myspt[0].ploop_init_nu(pkz0,pkz1,pkz2,pkz0p,pkz1p,pkz2p,ploopr,ploopp, zvals, *N_z, vars, mod, k0);
+
+      }
+
+        double myscalef[*N_z];
+        for(int i = 0; i<*N_z ; i++){
+            myscalef[i]= 1./(1.+zvals[i]);
+        }
+
+        /* Spline resulting arrays as functions of scale factor */
+        Spline mysr = CubicSpline(*N_z, myscalef, ploopr);
+        Spline mysp = CubicSpline(*N_z, myscalef, ploopp);
+
+
+        /* PART 3: main loop to initialise reaction */
+        int loop_nk = 60;
+        double react_tab[loop_nk],k2val[loop_nk];
+
+        for(int j = 0; j<*N_z; j++) {
+            vars[0] = 1./(1.+zvals[j]);
+
+            // initialise wCDM/LCDM lin growth for PS normalisation
+            iow.initnorm(vars,mod);
+
+
+            // Spherical collapse stuff
+            /// initialise delta_c(M), a_vir(M), delta_avir(M) and v(M)
+            status = myhalo[j].scol_init(vars,modcamb,mod); // real spherical collapse quantities
+            status |= myhalo[j].scol_initp(vars,modcamb); // pseudo spherical collapse quantities
+
+
+            // store modified sigma 8 at z=0
+            if(j == *N_z-1) {
+                modsig8[0] = vars[7];
+            }
+
+            // initialise k_star and mathcal{E}
+            myhalo[j].react_init_nu_multiz(vars,mysr,mysp,modcamb,modg);
+
+
+            // reaction
+            //#pragma omp parallel for
+            for(int i =0; i < loop_nk;  i ++) {
+                // 1.01 is put so that spline covers full range of input k values
+                k2val[i] = kvals[0] * exp(i*log(kvals[*N_k-1]*1.01/kvals[0])/(loop_nk-1.));
+                if(k2val[i]<0.01){
+                    react_tab[i] = 1.;
+                }
+                else {
+                    react_tab[i] = myhalo[j].reaction_nu(k2val[i], vars, modcamb);
+                }
+            }
+
+
+            Spline myreact = CubicSpline(loop_nk, k2val, react_tab);
+
+        /* PART 4: Store results to output arrays */
+            for(int i =0; i < *N_k;  i ++) {
+                output_react[i*(*N_z)+j] =  myreact(kvals[i]);
+                output_pl[i*(*N_z)+j] = mylps[j](kvals[i]);
+                if(*verbose > 1) {
+                    printf(" %e %e %e \n",zvals[j], kvals[i], mylps[j](kvals[i]));
+                }
+            }
+        }
+
+        return 0;
+  }
 }
